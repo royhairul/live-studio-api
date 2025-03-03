e5688e58c80adb44e8158b17be6f89a65721395c
royhairul <royhairulanam19@gmail.com>
2025-03-03 15:19:21 +0700

    feat: initialize main entry point for application

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"live-studio-api/config"
+	"live-studio-api/database"
+	"live-studio-api/routes"
+)
+
+func main() {
+	// Load konfigurasi
+	cfg := config.LoadConfig()
+
+
+	// Koneksi Database
+	database.ConnectDatabase(cfg)
+
+	// Inisialisasi router
+	r := routes.SetupRouter()
+
+	// Jalankan server
+	r.Run(fmt.Sprintf(":%s", cfg.ServerPort))
+}
