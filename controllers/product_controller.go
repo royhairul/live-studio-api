d5e076c3607bfed9df81d975d98ff45c719c0eff
royhairul <royhairulanam19@gmail.com>
2025-03-04 11:13:10 +0700

    feat: add response for handler index and create product

--- controllers/product_controller.go
+++ controllers/product_controller.go
@@ -12,19 +12,37 @@ import (
 func ProductIndex(c *gin.Context) {
 	var products []models.Product
 	database.DB.Find(&products)
+	
+	// Check if product list is empty
+	if len(products) == 0 {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "List Product is empty.",
+			"data": products,
+		})
+		return // Prevent sending multiple responses
+	}
 
-	c.JSON(http.StatusOK, products)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "List All Product",
+		"data": products,
+	})
 }
 
 // POST Create a Product
 func ProductCreate(c *gin.Context) {
 	var product models.Product
 	if err := c.ShouldBindJSON(&product); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Failed to create product",
+			"error": err.Error(),
+		})
 		return
 	}
 	database.DB.Create(&product)
-	c.JSON(http.StatusCreated, product)
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Product successfully created",
+		"data": product,
+	})
 }
 
 func ProductUpdate() {}
