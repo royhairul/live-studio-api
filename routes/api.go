6b14ea2b2a8bada1cf4230c9c1f52873e9e14446
royhairul <royhairulanam19@gmail.com>
2025-05-12 01:03:07 +0700

    feat: add route api for login, register, akun, host and studio

--- routes/api.go
+++ routes/api.go
@@ -9,8 +9,28 @@ import (
 func SetupRouter() *gin.Engine {
 	route := gin.Default()
 
+	// User Routes
+	route.POST("/api/login", controllers.Login)
+	route.POST("/api/register", controllers.Register)
+
+	// Product Routes
 	route.GET("/api/products/all", controllers.ProductIndex)
 	route.POST("/api/products/create", controllers.ProductCreate)
+
+	// Akun Routes
+	route.GET("/api/akun/all", controllers.AkunIndex)
+	route.POST("/api/akun/create", controllers.AkunCreate)
+
+	// Host Routes
+	route.GET("/api/host/all", controllers.HostIndex)
+	route.GET("/api/host/:id", controllers.HostShow)
+	route.POST("/api/host/create", controllers.HostCreate)
+	route.PUT("/api/host/:id/update", controllers.HostUpdate)
+	route.DELETE("/api/host/:id/delete", controllers.HostDelete)
+
+	// Studio Routes
+	route.GET("/api/studio/all", controllers.StudioIndex)
+	route.POST("/api/studio/create", controllers.StudioCreate)
 
 	return route
 }
