eaac3f153893f6d18aab4d6c080ef4344343e6a0
royhairul <royhairulanam19@gmail.com>
2025-05-27 19:57:38 +0700

    feat: add Email for user models

--- models/user.go
+++ models/user.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Username string `gorm:"type:varchar(100);not null"`
+	Email	 string `gorm:"type:varchar(100)"`
 	Password string `gorm:"type:varchar(100);not null"`
 	Role	 string	`gorm:"type:varchar(10);not null;default:'host'"`
 }
