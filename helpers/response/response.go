6c58701968da7730c97fe7ef2da88906a0375569
royhairul <royhairulanam19@gmail.com>
2025-07-02 20:16:12 +0700

    feat: update BaseResponse to support omitempty for field and enhance nil handling

--- helpers/response/response.go
+++ helpers/response/response.go
@@ -1,8 +1,10 @@
 package response
 
+import "reflect"
+
 type BaseResponse struct {
 	Message string `json:"message"`
-	Data    any    `json:"data"`
+	Data    any    `json:"data,omitempty"`
 }
 
 func NewBaseResponse(message string, data any) *BaseResponse {
@@ -11,8 +13,26 @@ func NewBaseResponse(message string, data any) *BaseResponse {
 			Message: message,
 		}
 	}
+
+	if isNil(data) {
+		data = makeEmptySliceOfSameType(data)
+	}
+
 	return &BaseResponse{
 		Message: message,
 		Data:    data,
 	}
+}
+
+func isNil(data interface{}) bool {
+	if data == nil {
+		return false
+	}
+	v := reflect.ValueOf(data)
+	return v.Kind() == reflect.Slice && v.IsNil()
+}
+
+func makeEmptySliceOfSameType(data interface{}) interface{} {
+	t := reflect.TypeOf(data)
+	return reflect.MakeSlice(t, 0, 0).Interface()
 }
