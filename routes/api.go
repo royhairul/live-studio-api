36516713b1119f4dca59cbdd016f373dbf0c4bd3
royhairul <royhairulanam19@gmail.com>
2025-07-19 13:35:05 +0700

    refactor: reorganize API routes and update CORS settings

--- routes/api.go
+++ routes/api.go
@@ -6,22 +6,26 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/royhairul/live-studio-api/controllers"
-	"github.com/royhairul/live-studio-api/internal/domains/account"
-	"github.com/royhairul/live-studio-api/internal/domains/attendance"
-	"github.com/royhairul/live-studio-api/internal/domains/finance"
-	"github.com/royhairul/live-studio-api/internal/domains/host"
-	"github.com/royhairul/live-studio-api/internal/domains/live"
-	"github.com/royhairul/live-studio-api/internal/domains/schedule"
-	"github.com/royhairul/live-studio-api/internal/domains/shift"
-	"github.com/royhairul/live-studio-api/internal/domains/studio"
-	"github.com/royhairul/live-studio-api/middleware"
+	"github.com/royhairul/live-studio-api/internal/middleware"
+	"go.uber.org/fx"
 )
+
+var RouteModule = fx.Module(
+	"router",
+	fx.Provide(SetupRouter),
+)
+
+func GroupAPI(router *gin.Engine) *gin.RouterGroup {
+	return router.Group("/api")
+}
 
 func SetupRouter() *gin.Engine {
 	route := gin.Default()
 
+	route.Group("/api")
+
 	route.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:4173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Role"},
 		ExposeHeaders:    []string{"Content-Length"},
@@ -42,12 +46,12 @@ func SetupRouter() *gin.Engine {
 	RegisterSuperAdminRoutes(route)
 
 	// Product Routes
-	route.GET("/api/products/all", controllers.ProductIndex)
-	route.POST("/api/products/create", controllers.ProductCreate)
+	// route.GET("/api/products/all", controllers.ProductIndex)
+	// route.POST("/api/products/create", controllers.ProductCreate)
 
 	// Akun Routes
-	route.GET("/api/account", controllers.AccountIndex)
-	route.POST("/api/account", controllers.AccountCreate)
+	// route.GET("/api/account", controllers.AccountIndex)
+	// route.POST("/api/account", controllers.AccountCreate)
 
 	// Host Routes
 	// route.GET("/api/host", controllers.HostIndex)
@@ -55,7 +59,7 @@ func SetupRouter() *gin.Engine {
 	// route.GET("/api/host/:id", controllers.HostShow)
 	// route.PUT("/api/host/:id", controllers.HostUpdate)
 	// route.DELETE("/api/host/:id", controllers.HostDelete)
-	route.GET("/api/host/:id/schedule", controllers.HostScheduleByHostID)
+	// route.GET("/api/host/:id/schedule", controllers.HostScheduleByHostID)
 
 	// Host by Studio Routes
 	// route.GET("/api/host/group-by-studio", controllers.HostGroupedByStudio)
@@ -84,27 +88,26 @@ func SetupRouter() *gin.Engine {
 	// route.DELETE("/api/studio/:id", controllers.StudioDelete)
 
 	// Roles
-	route.GET("api/role", controllers.RoleIndex)
-	route.POST("api/role", controllers.RoleCreate)
-	route.GET("api/role/:id", controllers.RoleShow)
-	route.PUT("api/role/:id", controllers.RoleUpdate)
-	route.DELETE("api/role/:id", controllers.RoleDelete)
+	// route.GET("api/role", controllers.RoleIndex)
+	// route.POST("api/role", controllers.RoleCreate)
+	// route.GET("api/role/:id", controllers.RoleShow)
+	// route.PUT("api/role/:id", controllers.RoleUpdate)
+	// route.DELETE("api/role/:id", controllers.RoleDelete)
 
 	// Permissions
-	route.GET("api/permission", controllers.PermissionIndex)
-	route.GET("api/permission/grouped", controllers.PermissionIndexGrouped)
+	// route.GET("api/permission", controllers.PermissionIndex)
+	// route.GET("api/permission/grouped", controllers.PermissionIndexGrouped)
 
 	// Finance
-	route.GET("api/finance/report", controllers.FinanceGetLiveReport)
+	// route.GET("api/finance/report", controllers.FinanceGetLiveReport)
 
-	shift.RegisterRoutes(route.Group("/api"))
-	studio.RegisterRoutes(route.Group("/api"))
-	host.RegisterRouter(route.Group("/api"))
-	schedule.RegisterRoutes(route.Group("/api"))
-	account.RegisterRouter(route.Group("/api"))
-	finance.RegisterRouter(route.Group("/api"))
-	attendance.RegisterRouter(route.Group("/api"))
-	live.RegisterRouter(route.Group("/api"))
+	// shift.RegisterRoutes(route.Group("/api"))
+	// studio.RegisterRoutes(route.Group("/api"))
+	// schedule.RegisterRoutes(route.Group("/api"))
+	// account.RegisterRouter(route.Group("/api"))
+	// finance.RegisterRouter(route.Group("/api"))
+	// attendance.RegisterRouter(route.Group("/api"))
+	// live.RegisterRouter(route.Group("/api"))
 
 	return route
 }
