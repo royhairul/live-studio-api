d5e77fc26e5d5e433a744eab9169c59d6f933bcb
royhairul <royhairulanam19@gmail.com>
2025-07-19 13:36:51 +0700

    feat: add shopee client  for creator, seller, and default clients

---
+++ internal/clients/shopee/provider.go
@@ -0,0 +1,24 @@
+package shopee
+
+import "github.com/royhairul/live-studio-api/internal/pkg/httpclient"
+
+func NewShopeeCreatorClient(client httpclient.Client) *ShopeeClient {
+	return &ShopeeClient{
+		Client:  client,
+		BaseURL: "https://creator.shopee.co.id",
+	}
+}
+
+func NewShopeeSellerClient(client httpclient.Client) *ShopeeClient {
+	return &ShopeeClient{
+		Client:  client,
+		BaseURL: "https://seller.shopee.co.id",
+	}
+}
+
+func NewShopeeDefaultClient(client httpclient.Client) *ShopeeClient {
+	return &ShopeeClient{
+		Client:  client,
+		BaseURL: "https://shopee.co.id",
+	}
+}
