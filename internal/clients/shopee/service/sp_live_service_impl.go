633f10a5d5c85c0816ff1914ef3931942ea70252
royhairul <royhairulanam19@gmail.com>
2025-07-19 13:25:43 +0700

    refactor: change shopee api from repository to service

--- internal/clients/shopee/service/sp_live_service_impl.go
+++ internal/clients/shopee/service/sp_live_service_impl.go
@@ -1,19 +1,53 @@
 package service
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/royhairul/live-studio-api/internal/clients/shopee"
 	"github.com/royhairul/live-studio-api/internal/clients/shopee/params"
-	"github.com/royhairul/live-studio-api/internal/clients/shopee/repository"
+	"go.uber.org/fx"
 )
 
-type ShopeeLiveServiceImpl struct {
-	repo repository.ShopeeLiveRepository
+type ShopeeLiveServiceDeps struct {
+	fx.In
+	ShopeeClient *shopee.ShopeeClient `name:"creatorShopeeClient"`
 }
 
-func NewShopeeLiveService(repo repository.ShopeeLiveRepository) ShopeeLiveService {
-	return &ShopeeLiveServiceImpl{repo}
+type ShopeeLiveServiceImpl struct {
+	ShopeeClient *shopee.ShopeeClient
+}
+
+func NewShopeeLiveService(deps ShopeeLiveServiceDeps) ShopeeLiveService {
+	return &ShopeeLiveServiceImpl{ShopeeClient: deps.ShopeeClient}
 }
 
 // GetShopeeLiveRealTime implements ShopeeLiveService.
 func (s ShopeeLiveServiceImpl) GetShopeeLiveRealTime(cookie string) ([]params.ShopeeLiveReportItemRT, error) {
-	return s.repo.GetShopeeLiveRealTime(cookie)
+	endpoint := "/supply/api/lm/sellercenter/realtime/sessionList"
+	query := map[string]string{
+		"page":     "1",
+		"pageSize": "10",
+		"name":     "",
+		"orderBy":  "",
+		"sort":     "desc",
+	}
+
+	req, err := s.ShopeeClient.NewShopeeRequest("GET", endpoint, query, nil, cookie)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request to %s: %w", endpoint, err)
+	}
+	log.Println("RESULT", req)
+
+	var result params.ShopeeApiResponse[params.ShopeeLiveRealTimeResponse]
+
+	if err := s.ShopeeClient.DoShopeeRequest(req, &result); err != nil {
+		return nil, fmt.Errorf("failed to do request to %s: %w", endpoint, err)
+	}
+
+	if result.Error != 0 {
+		return nil, fmt.Errorf(result.ErrorMsg)
+	}
+
+	return result.Data.List, nil
 }
