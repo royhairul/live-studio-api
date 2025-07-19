8d62e4a9b4f39fea971c5985689d80dcd273abfe
royhairul <royhairulanam19@gmail.com>
2025-07-19 13:33:26 +0700

    refactor: update to  return database and error

--- database/setup.go
+++ database/setup.go
@@ -11,7 +11,7 @@ import (
 
 var DB *gorm.DB
 
-func ConnectDatabase(cfg *config.Config) {
+func ConnectDatabase(cfg *config.Config) (*gorm.DB, error) {
 	// Buat DSN dari konfigurasi
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
@@ -22,10 +22,13 @@ func ConnectDatabase(cfg *config.Config) {
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
+		return nil, err
 	}
 
 	log.Println("Connected to database succesfully")
 	database.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
 
 	DB = database
+
+	return database, nil
 }
