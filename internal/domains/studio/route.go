c66fdf00505eaf3d667f400dbb977849728bac83
royhairul <royhairulanam19@gmail.com>
2025-08-07 23:22:51 +0700

    refactor: implement dependency injection for studio

--- internal/domains/studio/route.go
+++ internal/domains/studio/route.go
@@ -2,26 +2,17 @@ package studio
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
-	"github.com/royhairul/live-studio-api/database"
 	"github.com/royhairul/live-studio-api/internal/domains/studio/controller"
-	"github.com/royhairul/live-studio-api/internal/domains/studio/repository"
-	"github.com/royhairul/live-studio-api/internal/domains/studio/service"
 )
 
-func RegisterRoutes(router *gin.RouterGroup) {
+func RegisterRoutes(router *gin.RouterGroup, controller controller.StudioController) {
 	// TODO: define routes
-	validate := validator.New()
-	studioRepo := repository.NewStudioRepository(database.DB)
-	studioService := service.NewStudioService(studioRepo)
-	studioController := controller.NewStudioController(studioService, validate)
-
 	studioRouter := router.Group("/studio")
 	{
-		studioRouter.GET("", studioController.FindAll)
-		studioRouter.POST("", studioController.Create)
-		studioRouter.GET("/:id", studioController.FindByID)
-		studioRouter.PUT("/:id", studioController.Update)
-		studioRouter.DELETE("/:id", studioController.Delete)
+		studioRouter.GET("", controller.FindAll)
+		studioRouter.POST("", controller.Create)
+		studioRouter.GET("/:id", controller.FindByID)
+		studioRouter.PUT("/:id", controller.Update)
+		studioRouter.DELETE("/:id", controller.Delete)
 	}
 }
