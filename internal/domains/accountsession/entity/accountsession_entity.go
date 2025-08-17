5918e05c5687111cf18917d1effecf48830e7d45
royhairul <royhairulanam19@gmail.com>
2025-08-17 16:28:03 +0700

    feat: add calculate total gmv sales and paid and fix response

--- internal/domains/accountsession/entity/accountsession_entity.go
+++ internal/domains/accountsession/entity/accountsession_entity.go
@@ -18,3 +18,17 @@ type Accountsession struct {
 	GMVPaidStart  uint
 	GMVPaidEnd    uint
 }
+
+func (a *Accountsession) TotalPaid() uint {
+	if a.GMVPaidEnd > a.GMVPaidStart {
+		return a.GMVPaidEnd - a.GMVPaidStart
+	}
+	return 0
+}
+
+func (a Accountsession) TotalSales() uint {
+	if a.GMVSalesEnd > a.GMVSalesStart {
+		return a.GMVSalesEnd - a.GMVSalesStart
+	}
+	return 0
+}
