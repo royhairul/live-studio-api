1dac04863a7f48396f18382ef3c1ccc35c5e17b9
royhairul <royhairulanam19@gmail.com>
2025-08-25 07:15:22 +0700

    fix: update for handler route to documentation

--- routes/api.go
+++ routes/api.go
@@ -19,17 +19,44 @@ func GroupAPI(router *gin.Engine) *gin.RouterGroup {
 
 func SetupRouter() *gin.Engine {
 	route := gin.Default()
-
-	route.Group("/api")
-
 	route.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:4173"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:4173", "http://localhost:5174"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Role"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
+
+	route.Group("/api")
+
+	// Serve OpenAPI JSON
+	route.StaticFile("/docs/openapi.json", "./docs/LiveStudio.openapi.json")
+
+	// Serve Swagger UI
+	route.GET("/docs", func(c *gin.Context) {
+		c.Data(200, "text/html; charset=utf-8", []byte(`
+		<!DOCTYPE html>
+		<html>
+		<head>
+		  <title>Live Studio API Docs</title>
+		  <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+		</head>
+		<body>
+		  <div id="swagger-ui"></div>
+		  <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+		  <script>
+		    window.onload = () => {
+		      SwaggerUIBundle({
+		        url: '/docs/openapi.json', // sesuaikan dengan StaticFile
+		        dom_id: '#swagger-ui',
+		      });
+		    };
+		  </script>
+		</body>
+		</html>
+		`))
+	})
 
 	// User Management Routes
 	RegisterSuperAdminRoutes(route)
