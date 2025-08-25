60be07271238b5651694930cd5c6eab32029ba3b
royhairul <royhairulanam19@gmail.com>
2025-08-25 07:14:05 +0700

    feat: add helper for timehandler get date now and update for time now, parse date range

--- helpers/timehandler/get_date_now.go
+++ helpers/timehandler/get_date_now.go
@@ -2,8 +2,8 @@ package timehandler
 
 import "time"
 
-func TimeNow() *time.Time {
+func DateNow() *string {
 	loc, _ := time.LoadLocation("Asia/Jakarta")
-	now := time.Now().In(loc)
+	now := time.Now().In(loc).Format("2006-01-02")
 	return &now
 }
--- helpers/timehandler/parse_date_range.go
+++ helpers/timehandler/parse_date_range.go
@@ -16,5 +16,8 @@ func ParseDateRange(startDate string, endDate string) (*time.Time, *time.Time, e
 		return nil, nil, fmt.Errorf("invalid end date: %w", err)
 	}
 
+	tmp := end.Add(24*time.Hour - time.Nanosecond)
+	end = &tmp
+
 	return start, end, nil
 }
