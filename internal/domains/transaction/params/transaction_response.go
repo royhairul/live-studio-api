6f56d9a19200bf4d7d01a5adcb382f99cb8cec2f
royhairul <royhairulanam19@gmail.com>
2025-09-05 00:15:19 +0700

    fix: update transaction response

--- internal/domains/transaction/params/transaction_response.go
+++ internal/domains/transaction/params/transaction_response.go
@@ -1,16 +1,25 @@
 package params
 
 import (
+	"time"
+
 	"github.com/royhairul/live-studio-api/internal/domains/transaction/entity"
 
 	orderparams "github.com/royhairul/live-studio-api/internal/domains/order/params"
 )
+
+type Commission struct {
+	Total   uint `json:"total"`
+	Pending uint `json:"pending"`
+	Paid    uint `json:"paid"`
+}
 
 type TransactionResponse struct {
 	// TODO: add response fields
 	AccountID   uint                        `json:"account_id"`
 	AccountName string                      `json:"account_name"`
 	Total       int                         `json:"total"`
+	Commission  Commission                  `json:"commission"`
 	List        []TransactionDetailResponse `json:"list"`
 }
 
@@ -24,12 +33,14 @@ type CreatedTransactionResponse struct {
 type TransactionDetailResponse struct {
 	// TODO: add response fields
 	ID                              int64                       `json:"id"`
+	AccountID                       uint                        `json:"account_id"`
+	AccountName                     string                      `json:"account_name"`
 	UniqueID                        string                      `json:"unique_id"`
 	Status                          string                      `json:"status"`
 	EstimatedTotalCommission        int64                       `json:"est_total_comission"`
 	EstimatedTotalCommissionWithMCN int64                       `json:"est_total_comission_with_mcn"`
-	PurchaseTime                    int64                       `json:"purchase_time"`
-	CompleteTime                    int64                       `json:"complete_time"`
+	PurchaseTime                    *time.Time                  `json:"purchase_time"`
+	CompleteTime                    *time.Time                  `json:"complete_time"`
 	Orders                          []orderparams.OrderResponse `json:"orders"`
 }
 
@@ -37,6 +48,8 @@ func NewTransactionDetailResponse(transaction *entity.Transaction) *TransactionD
 	return &TransactionDetailResponse{
 		ID:                       transaction.ID,
 		UniqueID:                 transaction.UniqueID,
+		AccountID:                transaction.Account.ID,
+		AccountName:              transaction.Account.Name,
 		Status:                   transaction.Status,
 		EstimatedTotalCommission: transaction.EstimatedTotalCommission,
 		PurchaseTime:             transaction.PurchaseTime,
