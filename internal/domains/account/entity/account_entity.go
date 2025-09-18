22172d28149982a2a0751e4db277c0e77bf6ed9d
royhairul <royhairulanam19@gmail.com>
2025-09-18 10:04:49 +0700

    fix: update entity studio relation to account

--- internal/domains/account/entity/account_entity.go
+++ internal/domains/account/entity/account_entity.go
@@ -1,20 +1,20 @@
 package entity
 
 import (
-	models "github.com/royhairul/live-studio-api/models"
+	studioentity "github.com/royhairul/live-studio-api/internal/domains/studio/entity"
 	"gorm.io/gorm"
 )
 
 type Account struct {
 	gorm.Model
-	Name     string        `gorm:"type:varchar(100);not null"`
-	UniqueID string        `gorm:"type:varchar(20);not null;unique"`
-	Username string        `gorm:"type:varchar(100);not null"`
-	Password string        `gorm:"type:varchar(100)"`
-	Email    string        `gorm:"type:varchar(100);not null"`
-	Platform string        `gorm:"type:varchar(100);not null"`
-	Cookie   string        `gorm:"type:text;not null"`
-	Device   string        `gorm:"type:text"`
-	StudioID uint16        `gorm:"not null"`
-	Studio   models.Studio `gorm:"foreignKey:StudioID;references:ID"`
+	Name     string              `gorm:"type:varchar(100);not null"`
+	UniqueID string              `gorm:"type:varchar(20);not null;unique"`
+	Username string              `gorm:"type:varchar(100);not null"`
+	Password string              `gorm:"type:varchar(100)"`
+	Email    string              `gorm:"type:varchar(100);not null"`
+	Platform string              `gorm:"type:varchar(100);not null"`
+	Cookie   string              `gorm:"type:text;not null"`
+	Device   string              `gorm:"type:text"`
+	StudioID uint16              `gorm:"not null"`
+	Studio   studioentity.Studio `gorm:"foreignKey:StudioID;references:ID"`
 }
