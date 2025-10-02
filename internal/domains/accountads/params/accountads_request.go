0c1bdb61bf52f41ff8774918dd34e6198d657ff9
royhairul <royhairulanam19@gmail.com>
2025-10-02 17:59:47 +0700

    fix: update account ads response

--- internal/domains/accountads/params/accountads_request.go
+++ internal/domains/accountads/params/accountads_request.go
@@ -11,7 +11,7 @@ type CreateAccountadsRequest struct {
 }
 
 type UpdateAccountadsRequest struct {
-	AccountID *uint   `json:"accountid" validate:"required"`
+	AccountID *uint   `json:"account_id" validate:"required"`
 	Date      *string `json:"date" validate:"required"`
 	Ads       *uint   `json:"ads" vaildate:"required"`
 }
