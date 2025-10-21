abb52a08f69670015b2632e52b0df5d77a7935ad
royhairul <royhairulanam19@gmail.com>
2025-10-21 09:26:16 +0700

    feat: add helper for format date from int64 to string date

--- internal/pkg/timehandler/parse_date.go
+++ internal/pkg/timehandler/parse_date.go
@@ -7,17 +7,19 @@ import (
 	"github.com/royhairul/live-studio-api/internal/pkg/constants"
 )
 
+// ParseDate parses a date string into a time.Time object based on the defined layout.
 func ParseDate(str string) (*time.Time, error) {
 	layout := constants.LayoutYYMMDD
 
 	parsedDate, err := time.Parse(layout, str)
 	if err != nil {
-		return nil, fmt.Errorf("Error parsing date: %w", err)
+		return nil, fmt.Errorf("error parsing date: %w", err)
 	}
 
 	return &parsedDate, nil
 }
 
+// ParseInt64Date converts a Unix timestamp (in seconds) into a *time.Time object (in UTC).
 func ParseInt64Date(timestamp int64) *time.Time {
 	if timestamp <= 0 {
 		return nil
@@ -27,8 +29,22 @@ func ParseInt64Date(timestamp int64) *time.Time {
 	return &t
 }
 
+// FormatDate formats a *time.Time object into a string using the defined layout.
 func FormatDate(date *time.Time) string {
-	layout := constants.LayoutYYMMDD
+	if date == nil {
+		return ""
+	}
 
+	layout := constants.LayoutYYMMDD
 	return date.Format(layout)
+}
+
+// FormatInt64Date converts a Unix timestamp (in seconds) directly into a formatted date string.
+func FormatInt64Date(timestamp int64) string {
+	if timestamp <= 0 {
+		return ""
+	}
+
+	layout := constants.LayoutYYMMDD
+	return time.Unix(timestamp, 0).UTC().Format(layout)
 }
