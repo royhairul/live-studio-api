81fd0555b8b147a4efdad1d9d6cf08fbd7e56ece
royhairul <royhairulanam19@gmail.com>
2025-11-24 09:22:56 +0700

    fix: validating for auth request

--- internal/domains/auth/params/auth_request.go
+++ internal/domains/auth/params/auth_request.go
@@ -1,8 +1,8 @@
 package params
 
 type AuthBase struct {
-	Email    string `json:"email" binding:"required,min=3"`
-	Password string `json:"password" binding:"required,min=3"`
+	Email    string `json:"email" validate:"required,min=3"`
+	Password string `json:"password" validate:"required,min=3"`
 }
 
 type LoginRequest struct {
@@ -11,19 +11,19 @@ type LoginRequest struct {
 
 type RegisterRequest struct {
 	AuthBase
-	Name string `json:"name" binding:"required,min=3"`
+	Name string `json:"name" validate:"required,min=3"`
 }
 
 type ForgotPasswordRequest struct {
-	Email string `json:"email" binding:"required,min=3"`
+	Email string `json:"email" validate:"required,min=3"`
 }
 
 type ResetPasswordRequest struct {
-	OTP             string `json:"otp" binding:"required,min=3"`
-	Password        string `json:"password" binding:"required,min=3"`
-	ConfirmPassword string `json:"confirmPassword" binding:"required,min=3"`
+	OTP             string `json:"otp" validate:"required,min=3"`
+	Password        string `json:"password" validate:"required,min=3"`
+	ConfirmPassword string `json:"confirmPassword" validate:"required,min=3"`
 }
 
 type VerifyOTPRequest struct {
-	OTP string `json:"otp" binding:"required,min=3"`
+	OTP string `json:"otp" validate:"required,min=3"`
 }
